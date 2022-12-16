9111a52f068380a3dc93e0f1675d6f266237fa23
Jules <jalvord19@gmail.com>
2022-12-16 13:39:55 -0600

    wide contact info for parents

---
+++ models/build/edfi_3/parents/bld_ef3__parent_wide_emails.sql
@@ -0,0 +1,22 @@
+stg_parent_emails as (
+    select * from {{ ref('stg_ef3__parents__emails') }}
+),
+parent_email_types as (
+    select * from {{ ref('xwalk_parent_email_types') }}
+),
+emails_wide as (
+  select 
+    k_parent,
+    {{ dbt_utils.pivot(
+      'normalized_email_type',
+      dbt_utils.get_column_values(ref('xwalk_parent_email_types'), 'normalized_email_type'),
+      agg='max',
+      then_value='email_address',
+      suffix='_email_address'
+    ) }}
+  from stg_parent_emails
+  join parent_email_types 
+    on stg_parent_emails.email_type = parent_email_types.original_email_type
+  group by k_parent
+)
+select * from emails_wide
---
+++ models/build/edfi_3/parents/bld_ef3__parent_wide_phone_numbers.sql
@@ -0,0 +1,22 @@
+stg_parent_phones as (
+    select * from {{ ref('stg_ef3__parents__telephones') }}
+),
+parent_phone_number_types as (
+    select * from {{ ref('xwalk_parent_phone_number_types') }}
+),
+phones_wide as (
+  select 
+    k_parent,
+    {{ dbt_utils.pivot(
+      'normalized_phone_number_type',
+      dbt_utils.get_column_values(ref('xwalk_parent_phone_number_types'), 'normalized_phone_number_type'),
+      agg='max',
+      then_value='phone_number',
+      suffix='_phone_number'
+    ) }}
+  from stg_parent_phones
+  join parent_phone_number_types
+    on stg_parent_phones.phone_number_type = parent_phone_number_types.original_phone_number_type
+  group by k_parent
+)
+select * from phones_wide
