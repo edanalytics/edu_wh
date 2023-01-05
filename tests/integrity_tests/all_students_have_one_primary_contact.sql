660ea70345873b7a86eb47225b09fcac8601be78
Jules <jalvord19@gmail.com>
2023-01-05 11:22:23 -0600

    change to school year

--- tests/integrity_tests/all_students_have_one_primary_contact.sql
+++ tests/integrity_tests/all_students_have_one_primary_contact.sql
@@ -9,9 +9,9 @@ Find students who have multiple primary contacts in the student parent associati
 }}
 -- find students with multiple primary contacts
 with stu_with_multiple_primary_contacts as (
-  select tenant_code, api_year, k_student, sum(is_primary_contact::int) as n_primary_contacts
+  select tenant_code, school_year, k_student, sum(is_primary_contact::int) as n_primary_contacts
   from {{ ref('fct_student_parent_association') }}
-  group by tenant_code, api_year, k_student
+  group by tenant_code, school_year, k_student
   having n_primary_contacts > 1
 )
 select * from stu_with_multiple_primary_contacts
