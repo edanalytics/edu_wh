265258fa01a7a53cb517c2f0e8f3f603833ebe52
rlittle08 <rlittle@edanalytics.org>
2023-01-18 09:41:33 -0600

    bld array of program services, add to program assoc table

--- models/core_warehouse/fct_student_special_education_program_association.sql
+++ models/core_warehouse/fct_student_special_education_program_association.sql
@@ -1,7 +1,7 @@
 {{
   config(
     post_hook=[
-        "alter table {{ this }} add primary key (k_student, k_student_xyear, k_program, k_school, spec_ed_program_begin_date)",
+        "alter table {{ this }} add primary key (k_student, k_student_xyear, k_program, spec_ed_program_begin_date)",
         "alter table {{ this }} add constraint fk_{{ this.name }}_student foreign key (k_student) references {{ ref('dim_student') }}",
         "alter table {{ this }} add constraint fk_{{ this.name }}_program foreign key (k_program) references {{ ref('dim_program') }}",
     ]
@@ -18,6 +18,10 @@ dim_student as (
 
 dim_program as (
     select * from {{ ref('dim_program') }}
+),
+
+bld_program_services as (
+    select * From {{ ref ('bld_ef3__stu_spec_ed__program_services')}}
 ),
 
 formatted as (
@@ -46,13 +50,21 @@ formatted as (
         stg_stu_spec_ed.participation_status_end_date,
         stg_stu_spec_ed.participation_status,
         stg_stu_spec_ed.reason_exited,
-        stg_stu_spec_ed.special_education_setting
+        stg_stu_spec_ed.special_education_setting,
+        bld_program_services.special_education_program_services
         {{ edu_edfi_source.extract_extension(model_name='stg_ef3__student_special_education_program_associations', flatten=False) }}
     from stg_stu_spec_ed
     inner join dim_student
         on stg_stu_spec_ed.k_student = dim_student.k_student
     inner join dim_program
         on stg_stu_spec_ed.k_program = dim_program.k_program
+    -- left join because not all special ed programs include services (and they're optional in EdFi)
+    left join bld_program_services 
+        on stg_stu_spec_ed.k_student = bld_program_services.k_student
+        and stg_stu_spec_ed.k_program = bld_program_services.k_program
+        and equal_null(stg_stu_spec_ed.k_lea, bld_program_services.k_lea)
+        and equal_null(stg_stu_spec_ed.k_school, bld_program_services.k_school)
+        and stg_stu_spec_ed.spec_ed_program_begin_date = bld_program_services.spec_ed_program_begin_date
 )
 
 select * from formatted
