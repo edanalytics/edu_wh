17617715273c069e1938a60325bee3a26bc682d5
Jules <jalvord19@gmail.com>
2023-01-26 17:23:05 -0600

    change primary key

--- models/core_warehouse/fct_student_parent_association.sql
+++ models/core_warehouse/fct_student_parent_association.sql
@@ -1,7 +1,7 @@
 {{
   config(
     post_hook=[
-        "alter table {{ this }} add primary key (k_student, k_parent, school_year)",
+        "alter table {{ this }} add primary key (k_student, k_parent)",
         "alter table {{ this }} add constraint fk_{{ this.name }}_student foreign key (k_student) references {{ ref('dim_student') }}",
         "alter table {{ this }} add constraint fk_{{ this.name }}_parent foreign key (k_parent) references {{ ref('dim_parent') }}",
     ]
