368cc9187cd2f727fbde4702a31c6cbeca441a9a
Jules <jalvord19@gmail.com>
2023-02-24 15:57:31 -0600

    unfinished first draft of stu discipline incident behaviors

---
+++ models/core_warehouse/fct_student_discipline_incident_behaviors.sql
@@ -0,0 +1,52 @@
+with stg_stu_discipline_incident_behaviors as (
+    select * from {{ ref('stg_ef3__student_discipline_incident_behaviors') }}
+),
+stg_stu_discipline_non_offenders as (
+    select * from {{ ref('stg_ef3__student_discipline_non_offenders') }}
+),
+dim_student as (
+    select * from {{ ref('dim_student') }}
+),
+dim_school as (
+    select * from {{ ref('dim_school') }}
+),
+stack_discipline_incidents as (
+    select
+        stg_stu_discipline_incident_behaviors.k_student,
+        stg_stu_discipline_incident_behaviors.k_discipline_incident,
+        stg_stu_discipline_incident_behaviors.incident_id,
+        stg_stu_discipline_incident_behaviors.behavior_type,
+        stg_stu_discipline_incident_behaviors.behavior_detailed_description,
+        -- todo: how do I deal with the list of participation codes
+        -- don't want to change the grain
+        -- do I pull one out somehow? by severity?
+        true as is_offender
+    from stg_stu_discipline_incident_behaviors
+
+    union all 
+
+    select
+        stg_stu_discipline_non_offenders.k_student,
+        stg_stu_discipline_non_offenders.k_discipline_incident,
+        stg_stu_discipline_non_offenders.incident_id,
+        null as behavior_type,
+        null as behavior_detailed_description,
+        -- todo: how do I deal with the list of participation codes
+        -- don't want to change the grain
+        -- do I pull one out somehow? by severity?
+        false as is_offender
+    from stg_stu_discipline_non_offenders
+),
+formatted as (
+    select 
+        dim_student.k_student,
+        stack_discipline_incidents.k_discipline_incident,
+        stack_discipline_incidents.incident_id,
+        stack_discipline_incidents.behavior_type,
+        stack_discipline_incidents.behavior_detailed_description,
+        stack_discipline_incidents.is_offender
+    from stack_discipline_incidents
+    join dim_student on stack_discipline_incidents.k_student = dim_student.k_student
+)
+select *
+from formatted
