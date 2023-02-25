b35c7b407d1470fba54bca406c634f38716aaffd
Jules <jalvord19@gmail.com>
2023-02-25 11:08:21 -0600

    slight changes to correct grain

--- models/core_warehouse/fct_student_discipline_incident_behaviors_actions.sql
+++ models/core_warehouse/fct_student_discipline_incident_behaviors_actions.sql
@@ -3,8 +3,8 @@
 with stu_discipline_incident_behaviors_actions as (
     select * from {{ ref('stg_ef3__discipline_actions__student_discipline_incident_behaviors') }}
 ),
-student_discipline_incident_behaviors as (
-    select * from {{ ref('stg_ef3__student_discipline_incident_behaviors') }}
+fct_stu_discipline_actions as (
+    select * from {{ ref('fct_student_discipline_actions') }}
 ),
 dim_student as (
     select * from {{ ref('dim_student') }}
@@ -12,18 +12,21 @@ dim_student as (
 dim_school as (
     select * from {{ ref('dim_school') }}
 ),
---todo: still hate this
--- this doesn't even match the grain of stu discipline actions because it doesn't include the actual action
 formatted as (
     select
         dim_student_incident.k_student as k_student__incident,
-        dim_school.k_school as k_school_incident,
+        dim_school.k_school as k_school__incident,
         dim_student_action.k_student as k_student__action,
         stu_discipline_incident_behaviors_actions.incident_id,
         stu_discipline_incident_behaviors_actions.behavior_type,
         stu_discipline_incident_behaviors_actions.discipline_action_id,
-        stu_discipline_incident_behaviors_actions.discipline_date
+        stu_discipline_incident_behaviors_actions.discipline_date,
+        fct_student_discipline_actions.discipline_action
     from stu_discipline_incident_behaviors_actions
+    join fct_student_discipline_actions
+        on stu_discipline_incident_behaviors_actions.k_student__action = fct_student_discipline_actions.k_student
+        and stu_discipline_incident_behaviors_actions.discipline_action_id = fct_student_discipline_actions.discipline_action_id
+        and stu_discipline_incident_behaviors_actions.discipline_date = fct_student_discipline_actions.discipline_date
     join dim_student as dim_student_incident
         on stu_discipline_incident_behaviors_actions.student_unique_id = dim_student_incident.student_unique_id
         and stu_discipline_incident_behaviors_actions.tenant_code = dim_student_incident.tenant_code
