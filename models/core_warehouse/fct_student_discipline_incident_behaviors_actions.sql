940f6a7e715b648e3cc11bf0d0dfc85bca15a782
Jules <jalvord19@gmail.com>
2023-03-01 14:55:49 -0600

    updates to properly associate

--- models/core_warehouse/fct_student_discipline_incident_behaviors_actions.sql
+++ models/core_warehouse/fct_student_discipline_incident_behaviors_actions.sql
@@ -18,31 +18,31 @@ dim_school as (
 formatted as (
     select
         dim_student_incident.k_student as k_student__incident,
-        dim_school.k_school as k_school__incident,
+        fct_student_discipline_incident_behaviors.k_discipline_incident,
         dim_student_action.k_student as k_student__action,
-        stu_discipline_incident_behaviors_actions.incident_id,
-        -- we force behavior type into the grain of this fct table
+        stu_discipline_incident_behaviors_actions.k_discipline_action
         fct_student_discipline_incident_behaviors.behavior_type,
-        stu_discipline_incident_behaviors_actions.discipline_action_id,
-        stu_discipline_incident_behaviors_actions.discipline_date,
         fct_student_discipline_actions.discipline_action
     from stu_discipline_incident_behaviors_actions
     join fct_student_discipline_actions
         on stu_discipline_incident_behaviors_actions.k_student = fct_student_discipline_actions.k_student
         and stu_discipline_incident_behaviors_actions.discipline_action_id = fct_student_discipline_actions.discipline_action_id
         and stu_discipline_incident_behaviors_actions.discipline_date = fct_student_discipline_actions.discipline_date
-    join fct_student_discipline_incident_behaviors
-        on stu_discipline_incident_behaviors_actions.k_student = fct_student_discipline_incident_behaviors.k_student
-        and stu_discipline_incident_behaviors_actions.incident_id = fct_student_discipline_incident_behaviors.incident_id
-        and ifnull(stu_discipline_incident_behaviors_actions.behavior_type, 1) = iff(stu_discipline_incident_behaviors_actions.behavior_type is null, 1, fct_student_discipline_incident_behaviors.behavior_type)
     join dim_student as dim_student_incident
         on stu_discipline_incident_behaviors_actions.student_unique_id = dim_student_incident.student_unique_id
         and stu_discipline_incident_behaviors_actions.api_year = dim_student_incident.school_year
         and stu_discipline_incident_behaviors_actions.tenant_code = dim_student_incident.tenant_code
     join dim_student as dim_student_action
         on stu_discipline_incident_behaviors_actions.k_student = dim_student_action.k_student
+    join fct_student_discipline_incident_behaviors
+        -- note joining against dim student and not k student of stg table because that is k student action
+        on dim_student_incident.k_student = fct_student_discipline_incident_behaviors.k_student
+        and stu_discipline_incident_behaviors_actions.incident_id = fct_student_discipline_incident_behaviors.incident_id
+        and ifnull(stu_discipline_incident_behaviors_actions.behavior_type, 1) = iff(stu_discipline_incident_behaviors_actions.behavior_type is null, 1, fct_student_discipline_incident_behaviors.behavior_type)
     join dim_school 
         on stu_discipline_incident_behaviors_actions.school_id = dim_school.school_id
         and stu_discipline_incident_behaviors_actions.tenant_code = dim_school.tenant_code
+    -- todo: subset to non-offenders? (does this already happen with merge on discipline actions?)
+    where fct_student_discipline_incident_behaviors.is_offender
 )
 select * from formatted
