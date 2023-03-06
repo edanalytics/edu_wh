fedd2a4a0c81cf0051a18f3ad785f5d6f7817214
Jules <jalvord19@gmail.com>
2023-03-06 13:12:19 -0600

    split stu discipline incidents to offender and not models

--- models/core_warehouse/fct_student_discipline_incident_behaviors.sql
+++ models/core_warehouse/fct_student_discipline_incident_behaviors.sql
@@ -1,9 +1,6 @@
 with stg_stu_discipline_incident_behaviors as (
     select * from {{ ref('stg_ef3__student_discipline_incident_behavior_associations') }}
 ),
-stg_stu_discipline_incident_non_offenders as (
-    select * from {{ ref('stg_ef3__student_discipline_incident_non_offender_associations') }}
-),
 dim_student as (
     select * from {{ ref('dim_student') }}
 ),
@@ -13,48 +10,34 @@ dim_school as (
 dim_discipline_incidents as (
     select * from {{ ref('dim_discipline_incidents') }}
 ),
-stack_discipline_incidents as (
+participation_codes as (
     select
-        stg_stu_discipline_incident_behaviors.k_student,
-        stg_stu_discipline_incident_behaviors.k_school,
-        stg_stu_discipline_incident_behaviors.k_discipline_incident,
-        stg_stu_discipline_incident_behaviors.incident_id,
-        stg_stu_discipline_incident_behaviors.behavior_type,
-        stg_stu_discipline_incident_behaviors.behavior_detailed_description,
-        -- todo: how do I deal with the list of participation codes
-        -- don't want to change the grain
-        -- do I pull one out somehow? by severity?
-        true as is_offender
-    from stg_stu_discipline_incident_behaviors
-
-    union all 
-
-    select
-        stg_stu_discipline_incident_non_offenders.k_student,
-        stg_stu_discipline_incident_non_offenders.k_school,
-        stg_stu_discipline_incident_non_offenders.k_discipline_incident,
-        stg_stu_discipline_incident_non_offenders.incident_id,
-        null as behavior_type,
-        null as behavior_detailed_description,
-        -- todo: how do I deal with the list of participation codes
-        -- don't want to change the grain
-        -- do I pull one out somehow? by severity?
-        false as is_offender
-    from stg_stu_discipline_incident_non_offenders
+        k_student,
+        k_discipline_incident,
+        array_agg(participation_code) as participation_codes_array
+    from {{ ref('stg_ef3__student_discipline_incident_behavior_associations__participation_codes') }}
+    group by k_student, k_discipline_incident
 ),
 formatted as (
     select 
         dim_student.k_student,
         dim_school.k_school,
         dim_discipline_incidents.k_discipline_incident,
-        stack_discipline_incidents.incident_id,
-        stack_discipline_incidents.behavior_type,
-        stack_discipline_incidents.behavior_detailed_description,
-        stack_discipline_incidents.is_offender
-    from stack_discipline_incidents
-    join dim_student on stack_discipline_incidents.k_student = dim_student.k_student
-    join dim_school on stack_discipline_incidents.k_school = dim_school.k_school
-    join dim_discipline_incidents on stack_discipline_incidents.k_discipline_incident = dim_discipline_incidents.k_discipline_incident
+        stg_stu_discipline_incident_behaviors.tenant_code,
+        stg_stu_discipline_incident_behaviors.incident_id,
+        stg_stu_discipline_incident_behaviors.behavior_type,
+        stg_stu_discipline_incident_behaviors.behavior_detailed_description,
+        true as is_offender,
+        -- todo: name of this col?
+        {{ extract_descriptor('v_discipline_incident_participation_codes[0]:disciplineIncidentParticipationCodeDescriptor::string') }} as participation_code,
+        participation_codes.participation_codes_array
+    from stg_stu_discipline_incident_behaviors
+    join participation_codes 
+        on stg_stu_discipline_incident_behaviors.k_student = participation_codes.k_student
+        and stg_stu_discipline_incident_behaviors.k_discipline_incident = participation_codes.k_discipline_incident
+    join dim_student on stg_stu_discipline_incident_behaviors.k_student = dim_student.k_student
+    join dim_school on stg_stu_discipline_incident_behaviors.k_school = dim_school.k_school
+    join dim_discipline_incidents on stg_stu_discipline_incident_behaviors.k_discipline_incident = dim_discipline_incidents.k_discipline_incident
 )
 select *
 from formatted
---
+++ models/core_warehouse/fct_student_discipline_incident_non_offenders.sql
@@ -0,0 +1,41 @@
+with stg_stu_discipline_incident_non_offenders as (
+    select * from {{ ref('stg_ef3__student_discipline_incident_non_offender_associations') }}
+),
+dim_student as (
+    select * from {{ ref('dim_student') }}
+),
+dim_school as (
+    select * from {{ ref('dim_school') }}
+),
+dim_discipline_incidents as (
+    select * from {{ ref('dim_discipline_incidents') }}
+),
+participation_codes as (
+    select
+        k_student,
+        k_discipline_incident,
+        array_agg(participation_code) as participation_codes_array
+    from {{ ref('stg_ef3__student_discipline_incident_non_offender_associations__participation_codes') }}
+    group by k_student, k_discipline_incident
+),
+formatted as (
+    select 
+        dim_student.k_student,
+        dim_school.k_school,
+        dim_discipline_incidents.k_discipline_incident,
+        stg_stu_discipline_incident_non_offenders.tenant_code,
+        stg_stu_discipline_incident_non_offenders.incident_id,
+        false as is_offender,
+        -- todo: name of this col?
+        {{ extract_descriptor('v_discipline_incident_participation_codes[0]:disciplineIncidentParticipationCodeDescriptor::string') }} as participation_code,
+        participation_codes.participation_codes_array
+    from stg_stu_discipline_incident_non_offenders
+    join participation_codes 
+        on stg_stu_discipline_incident_non_offenders.k_student = participation_codes.k_student
+        and stg_stu_discipline_incident_non_offenders.k_discipline_incident = participation_codes.k_discipline_incident
+    join dim_student on stg_stu_discipline_incident_non_offenders.k_student = dim_student.k_student
+    join dim_school on stg_stu_discipline_incident_non_offenders.k_school = dim_school.k_school
+    join dim_discipline_incidents on stg_stu_discipline_incident_non_offenders.k_discipline_incident = dim_discipline_incidents.k_discipline_incident
+)
+select *
+from formatted
