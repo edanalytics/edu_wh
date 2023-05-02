5a6de6476aa8e1623a2b431edf309535bd580c6c
sleblanc23 <56237580+sleblanc23@users.noreply.github.com>
2023-05-02 17:42:31 -0500

    Feature/accordian columns coalesce (#53)
    * Add optional coalesce function to accordion_columns macro
    
    * Added coalesce values to characteristics and programs in dim_student

--- macros/accordion_columns.sql
+++ macros/accordion_columns.sql
@@ -6,8 +6,11 @@ be included in a table.
     in the table
   - source_alias: the alias of the table within the sql expression. If none (default)
     it will reuse the source_table name.
+  - coalesce_value: if this value is specified, a coalesce function will be added with
+    this value as the second parameter (to be returned if the value in the column is null).
+    If none (default), the coalesce function will not be added.
 -#}
-{% macro accordion_columns(source_table, exclude_columns, source_alias=none) %}
+{% macro accordion_columns(source_table, exclude_columns, source_alias=none, coalesce_value=none) %}
   {%- if source_alias is none -%}
    {%- set source_alias = source_table -%} 
   {%- endif -%}
@@ -15,7 +18,13 @@ be included in a table.
     ref(source_table),
       except=exclude_columns
     ) %}
-  {%- for col in keep_cols %}
-    {{ source_alias }}.{{ col }},
-  {%- endfor %}
+  {%- if coalesce_value is none %}
+    {%- for col in keep_cols %}
+      {{ source_alias }}.{{ col }},
+    {%- endfor %}
+  {%- else -%}
+    {%- for col in keep_cols %}
+      coalesce({{ source_alias }}.{{ col }}, {{ coalesce_value }}) as {{ col }},
+    {%- endfor %}  
+  {%- endif -%}
 {% endmacro %}
