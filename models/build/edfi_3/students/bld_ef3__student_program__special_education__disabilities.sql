9e58685288f2608cacd809947c6d84909949881c
Ryan Aguilar <raguilar@edanalytics.org>
2023-05-10 11:09:59 -0500

    explicitly select columns

--- models/build/edfi_3/students/bld_ef3__student_program__special_education__disabilities.sql
+++ models/build/edfi_3/students/bld_ef3__student_program__special_education__disabilities.sql
@@ -3,7 +3,19 @@ with stage_disabilities as (
 ),
 
 primary_disability as (
-	select distinct *
+	select distinct
+		tenant_code,
+		api_year,
+		k_student,
+		k_student_year,
+		ed_org_id,
+		k_lea,
+		k_school,
+		disability_type,
+		disabilitiy_source_type,
+		disability_diagnosis,
+		order_of_disability,
+		disability_designation
 	from stg_ef3__stu_spec_ed__disabilities
 	where order_of_disability = 1;
 )
