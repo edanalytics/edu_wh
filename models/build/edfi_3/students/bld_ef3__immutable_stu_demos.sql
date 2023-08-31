675e714cc618d4b82006be865fbe7158e3147607
rlittle08 <rlittle@edanalytics.org>
2023-08-31 13:09:42 -0500

    fix missing comma

--- models/build/edfi_3/students/bld_ef3__immutable_stu_demos.sql
+++ models/build/edfi_3/students/bld_ef3__immutable_stu_demos.sql
@@ -22,7 +22,7 @@ joined as (
         stg_student.last_name,
         concat(stg_student.last_name, ', ', stg_student.first_name,
             coalesce(' ' || left(stg_student.middle_name, 1), '')) as display_name,
-        concat(display_name, ' (', stg_student.student_unique_id, ')') as safe_display_name
+        concat(display_name, ' (', stg_student.student_unique_id, ')') as safe_display_name,
         stg_student.birth_date,
         stu_demos.gender,
         stu_races.race_ethnicity,
