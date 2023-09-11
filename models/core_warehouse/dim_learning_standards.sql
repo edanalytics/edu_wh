525f9dc22f75155e3806fa4a34d0c1b027cdd200
Angelica Lastra <angelicaalastra@gmail.com>
2023-09-11 15:26:09 -0700

    created dim table and referenced in fact table

---
+++ models/core_warehouse/dim_learning_standards.sql
@@ -0,0 +1,37 @@
+{{
+  config(
+    post_hook=[
+        "alter table {{ this }} add primary key (k_learning_standard)"
+    ]
+  )
+}}
+
+
+with stg_learning_standards as (
+    select * from {{ ref('stg_ef3__learning_standards') }}
+),
+formatted as (
+    select 
+        stg_learning_standards.k_learning_standard,
+        stg_learning_standards.tenant_code,
+        stg_learning_standards.api_year as school_year,
+        stg_learning_standards.learning_standard_id,
+        stg_learning_standards.v_learning_standard_identification_code,
+        stg_learning_standards.learning_standard_item_code,
+        stg_learning_standards.v_learning_standard_category,
+        stg_learning_standards.v_learning_standard_scope,
+        stg_learning_standards.v_academic_subjects,
+        stg_learning_standards.v_content_standard,
+        stg_learning_standards.course_title, 
+        stg_learning_standards.learning_standard_description,
+        stg_learning_standards.v_grade_level,
+        stg_learning_standards.parent_learning_standard_reference,
+        stg_learning_standards.success_criteria,
+        stg_learning_standards.namespace,
+        stg_learning_standards.uri,
+        stg_learning_standards.v_ext,
+        stg_learning_standards.academic_subject_descriptor
+    from stg_learning_standards
+)
+select * from formatted
+order by tenant_code, k_learning_standard
--- models/core_warehouse/fct_student_learning_standard_grades.sql
+++ models/core_warehouse/fct_student_learning_standard_grades.sql
@@ -10,11 +10,11 @@
   )
 }}
 
-with stg_grades as (
-    select * from {{ ref('stg_ef3__grades') }}
+with stg_grades_learning_standards as (
+    select * from {{ ref('stg_ef3__grades__learning_standards') }}
 ),
-stg_learning_standards as (
-    select * from {{ ref('stg_ef3__learning_standards') }}
+dim_learning_standards as (
+    select * from {{ ref('dim_learning_standards') }}
 ),
 dim_student as (
     select * from {{ ref('dim_student') }}
@@ -29,48 +29,31 @@ dim_course_section as (
     select * from {{ ref('dim_course_section') }}
 ),
 
---this has to be outside the staging model because it changes the grain of the table
---from k_student, k_school, k_grading_period, k_course_section, grade_type
---to k_student, k_school, k_grading_period, k_course_section, grade_type, learning_standard_id
-flattened as (
-    select
-        
-        stg_grades.* exclude(letter_grade_earned, numeric_grade_earned),
-        v_lsg.value:learningStandardReference:learningStandardId::string as learning_standard_id,
-        v_lsg.value:letterGradeEarned::string as letter_grade_earned,
-        v_lsg.value:numericGradeEarned::string as numeric_grade_earned,
-        {{ edu_edfi_source.extract_descriptor('v_lsg.value:performanceBaseConversionDescriptor::string') }} as performance_base_conversion_descriptor
-    from stg_grades,
-        lateral flatten(input=>v_learning_standard_grades, outer=>true) as v_lsg
-),
-
 formatted as (
     select 
         dim_student.k_student,
         dim_course_section.k_course_section,
         dim_school.k_school,
         dim_grading_period.k_grading_period,
-        stg_learning_standards.k_learning_standard,
-        stg_learning_standards.learning_standard_id,
-        stg_learning_standards.academic_subject_descriptor,
-        flattened.grading_period_descriptor,
-        flattened.grade_type,
-        flattened.tenant_code,
-        flattened.performance_base_conversion_descriptor as performance_base_conversion,
-        flattened.letter_grade_earned,
-        flattened.numeric_grade_earned
-    from flattened
-    join stg_learning_standards
-        on flattened.learning_standard_id = stg_learning_standards.learning_standard_id
-        and flattened.api_year = stg_learning_standards.api_year
-        and flattened.tenant_code = stg_learning_standards.tenant_code
+        dim_learning_standards.k_learning_standard,
+        dim_learning_standards.academic_subject_descriptor,
+        stg_grades_learning_standards.grade_type,
+        stg_grades_learning_standards.tenant_code,
+        stg_grades_learning_standards.performance_base_conversion_descriptor as performance_base_conversion,
+        stg_grades_learning_standards.letter_grade_earned,
+        stg_grades_learning_standards.numeric_grade_earned
+    from stg_grades_learning_standards
+    join dim_learning_standards
+        on stg_grades_learning_standards.learning_standard_id = dim_learning_standards.learning_standard_id
+        and stg_grades_learning_standards.api_year = dim_learning_standards.school_year
+        and stg_grades_learning_standards.tenant_code = dim_learning_standards.tenant_code
     join dim_student
-        on flattened.k_student = dim_student.k_student
+        on stg_grades_learning_standards.k_student = dim_student.k_student
     join dim_school 
-        on flattened.k_school = dim_school.k_school
+        on stg_grades_learning_standards.k_school = dim_school.k_school
     join dim_grading_period 
-        on flattened.k_grading_period = dim_grading_period.k_grading_period
+        on stg_grades_learning_standards.k_grading_period = dim_grading_period.k_grading_period
     join dim_course_section
-        on flattened.k_course_section = dim_course_section.k_course_section
+        on stg_grades_learning_standards.k_course_section = dim_course_section.k_course_section
 )
 select * from formatted
