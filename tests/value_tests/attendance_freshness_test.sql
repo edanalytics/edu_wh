0c5e410e304e61a24e9fdca4ebfa75804d0504fc
rlittle08 <rlittle@edanalytics.org>
2023-10-31 14:51:43 -0500

    comments in value tests

--- tests/value_tests/attendance_freshness_test.sql
+++ tests/value_tests/attendance_freshness_test.sql
@@ -1,3 +1,17 @@
+/*
+**What is this test?**
+This test finds records where attendance data for a tenant is stale (> 7 days old).
+This could be due to issues syncing from source system to ODS or from ODS
+to warehouse.
+
+**When is this important to resolve?**
+When up-to-date daily attendance is reported.
+
+**How to resolve?**
+Check whether there are errors pulling attendance records from ODS, 
+or if there are errors pushing data from source system to ODS.
+
+*/
 {{
   config(
       store_failures = true,
