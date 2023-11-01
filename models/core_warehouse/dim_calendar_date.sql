75a1fe9408f00cca431d747f50bd62616e419313
Jay Kaiser <jayckaiser@gmail.com>
2023-11-01 15:19:10 -0500

    Update dim_calendar_date.sql

--- models/core_warehouse/dim_calendar_date.sql
+++ models/core_warehouse/dim_calendar_date.sql
@@ -25,7 +25,7 @@ summarize_calendar_events as (
         stg_calendar_events.k_calendar_date,
         -- if there are multiple events on a day, having at least one 
         -- that counts as a school day applies to the whole day
-        sum(xwalk_calendar_events.is_school_day::integer) >= {{ var("edu:attendance:num_school_day_calendar_events") }} as is_school_day,
+        sum(xwalk_calendar_events.is_school_day::integer) >= {{ var("edu:attendance:num_school_day_calendar_events", 1) }} as is_school_day,
         array_agg(stg_calendar_events.calendar_event) as calendar_events_array
     from stg_calendar_events
     join xwalk_calendar_events
