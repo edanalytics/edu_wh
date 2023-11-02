8f17fb8440097932039a6c4e836d5220719e3324
sleblanc23 <sam23leblanc@gmail.com>
2023-11-02 16:29:02 -0500

    add is_active_cohort_association

--- models/core_warehouse/fct_student_cohort_association.sql
+++ models/core_warehouse/fct_student_cohort_association.sql
@@ -27,11 +27,22 @@ formatted as (
         dim_cohort.k_cohort,
         dim_cohort.k_lea,
         dim_cohort.k_school,
-
         stage.tenant_code,
         stage.school_year,
         stage.cohort_begin_date,
-        stage.cohort_end_date
+        stage.cohort_end_date,
+        -- create indicator for active cohort
+        iff(
+            -- is highest school year observed by tenant
+            stage.school_year = max(stage.school_year) 
+                over(partition by stage.tenant_code)
+            -- not yet exited
+            and (cohort_end_date is null
+                or cohort_end_date >= current_date())
+            -- enrollment has begun
+            and cohort_begin_date <= current_date(),
+            true, false
+        ) as is_active_cohort_association
 
         {{ edu_edfi_source.extract_extension(model_name='stg_ef3__student_cohort_associations', flatten=False) }}
 
