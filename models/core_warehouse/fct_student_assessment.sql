9fc03e6f9f4a1e1ae615be49fd1d122ab337b63b
rlittle08 <rlittle@edanalytics.org>
2023-11-03 15:56:37 -0500

    rearrange logic to bld and draft of historic assess models

--- models/core_warehouse/fct_student_assessment.sql
+++ models/core_warehouse/fct_student_assessment.sql
@@ -8,57 +8,19 @@
   )
 }}
 
-with student_assessments_long_results as (
-    select * from {{ ref('bld_ef3__student_assessments_long_results') }}
+with bld_stu_assess as (
+    select * from {{ ref('bld_ef3__student_assessment') }}
 ),
-student_assessments as (
-    select * from {{ ref('stg_ef3__student_assessments') }}
+dim_student as (
+    select * from {{ ref('dim_student') }}
 ),
-object_agg_other_results as (
+{# inner join to dim student to drop assess records without stu rosters. those assessments
+   will be represented in fct_student_historic_assessment #}
+joined as (
     select
-        k_student_assessment,
-        object_agg(original_score_name, score_result::variant) as v_other_results
-    from student_assessments_long_results
-    where normalized_score_name = 'other'
-    group by 1
-),
-student_assessments_wide as (
-    select
-        student_assessments.k_student_assessment,
-        student_assessments.k_assessment,
-        student_assessments.k_student,
-        student_assessments.tenant_code,
-        student_assessments.student_assessment_identifier,
-        student_assessments.serial_number,
-        school_year,
-        administration_date,
-        administration_end_date,
-        event_description,
-        administration_environment,
-        administration_language,
-        event_circumstance,
-        platform_type,
-        reason_not_tested,
-        retest_indicator,
-        when_assessed_grade_level,
-        v_other_results
-        {%- if not is_empty_model('xwalk_assessment_scores') -%},
-        {{ dbt_utils.pivot(
-            'normalized_score_name',
-            dbt_utils.get_column_values(ref('xwalk_assessment_scores'), 'normalized_score_name'),
-            then_value='score_result',
-            else_value='NULL',
-            agg='max',
-            quote_identifiers=False
-        ) }}
-        {%- endif %}
-    from student_assessments
-    left join student_assessments_long_results
-        on student_assessments.k_student_assessment = student_assessments_long_results.k_student_assessment
-        and student_assessments_long_results.normalized_score_name != 'other'
-    left join object_agg_other_results
-        on student_assessments.k_student_assessment = object_agg_other_results.k_student_assessment
-    {{ dbt_utils.group_by(n=18) }}
+      bld_stu_assess.*
+    from bld_stu_assess
+    join dim_student 
+      on bld_stu_assess.k_student = dim_student.k_student
 )
-select *
-from student_assessments_wide
+select * from joined
---
+++ models/core_warehouse/fct_student_historic_objective_assessment.sql
@@ -0,0 +1,47 @@
+{{
+  config(
+    post_hook=[
+        "alter table {{ this }} add primary key (k_student_objective_assessment)",
+        "alter table {{ this }} add constraint fk_{{ this.name }}_assessment foreign key (k_assessment) references {{ ref('dim_assessment') }}",
+        "alter table {{ this }} add constraint fk_{{ this.name }}_objective_assessment foreign key (k_objective_assessment) references {{ ref('dim_objective_assessment') }}",
+        "alter table {{ this }} add constraint fk_{{ this.name }}_student foreign key (k_student) references {{ ref('dim_student') }}"
+    ]
+  )
+}}
+
+with bld_stu_obj_assess as (
+    select * from {{ ref('bld_ef3__student_objective_assessment') }}
+),
+dim_student as (
+    select * from {{ ref('dim_student') }}
+),
+{# find most recent k_student value for each stu, this is the k_stu we'll link
+   historic assessments to #}
+most_recent_k_student as (
+    select
+        k_student,
+        k_student_xyear
+    from dim_student
+    qualify school_year = max(school_year) over (partition by k_student_xyear)
+),
+{# associate all historic assessment records with the most recent k_student for the given k_student_xyear.
+   this is the case for 
+    a) historic records that DO NOT have associated rostering data 
+    b) historic records that DO have associate rostering data
+#}
+joined as (
+    select
+      bld_stu_obj_assess.k_student_objective_assessment,
+      bld_stu_obj_assess.k_objective_assessment,
+      bld_stu_obj_assess.k_student_assessment,
+      bld_stu_obj_assess.k_assessment,
+      most_recent_k_student.k_student,
+      bld_stu_obj_assess.school_year as school_year__assessment,
+      {{ dbt_utils.star(from=ref('bld_ef3__student_objective_assessment'),
+                        except=['k_student_objective_assessment','k_objective_assessment','k_student_assessment','k_assessment','k_student','school_year'],
+                        relation_alias='bld_stu_obj_assess') }}
+    from bld_stu_obj_assess
+    join most_recent_k_student 
+      on bld_stu_obj_assess.k_student_xyear = most_recent_k_student.k_student_xyear
+)
+select * from joined
