6e78d262e795d706c581cc949916988af0daf0fd
ejoranlien <ejoranlien@edanalytics.org>
2023-11-13 18:51:59 -0600

    fix primary key docs, add enable flag

--- models/core_warehouse/fct_student_cohort_association.sql
+++ models/core_warehouse/fct_student_cohort_association.sql
@@ -1,7 +1,7 @@
 {{
   config(
     post_hook=[
-        "alter table {{ this }} add primary key (k_student, k_student_xyear, k_cohort, cohort_begin_date)",
+        "alter table {{ this }} add primary key (k_student, k_cohort, cohort_begin_date)",
         "alter table {{ this }} add constraint fk_{{ this.name }}_student foreign key (k_student) references {{ ref('dim_student') }}",
         "alter table {{ this }} add constraint fk_{{ this.name }}_cohort foreign key (k_cohort) references {{ ref('dim_cohort') }}",
     ]
