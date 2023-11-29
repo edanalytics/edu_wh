a870f6f44ae2c716492b51134fa56fd247362ab7
sleblanc23 <56237580+sleblanc23@users.noreply.github.com>
2023-11-28 18:59:26 -0600

    Feature/add warehouse models for cohorts (#78)
    * add warehouse models for cohorts
    
    * add is_active_cohort_association
    
    * add cohort tag
    
    * fix primary key docs, add enable flag
    
    ---------
    
    Co-authored-by: ejoranlien <ejoranlien@edanalytics.org>

---
+++ models/core_warehouse/dim_cohort.sql
@@ -0,0 +1,28 @@
+{{
+  config(
+    post_hook=[
+        "alter table {{ this }} add primary key (k_cohort)",
+    ]
+  )
+}}
+
+with stg_cohorts as (
+    select * from {{ ref('stg_ef3__cohorts') }}
+),
+formatted as (
+    select 
+        stg_cohorts.k_cohort,
+        stg_cohorts.k_lea,
+        stg_cohorts.k_school,
+        stg_cohorts.tenant_code,
+        stg_cohorts.school_year,
+        stg_cohorts.ed_org_id,
+        stg_cohorts.ed_org_type,
+        stg_cohorts.cohort_id,
+        stg_cohorts.cohort_description,
+        stg_cohorts.cohort_scope,
+        stg_cohorts.cohort_type
+    from stg_cohorts
+)
+select * from formatted
+order by tenant_code, school_year desc, k_cohort
