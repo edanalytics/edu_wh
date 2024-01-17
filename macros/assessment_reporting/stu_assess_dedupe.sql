26d56602e4d948c5ec06cc4efd5ae24efeecd4ba
rlittle08 <rlittle@edanalytics.org>
2024-01-17 11:20:24 -0600

    imlement new naming for dedupe macro & add configurable default dedupe

--- macros/assessment_reporting/stu_assess_dedupe.sql
+++ macros/assessment_reporting/stu_assess_dedupe.sql
@@ -1,60 +1,78 @@
-{# 
-This macro is used by fct_student_assessment_reporting and other assessment models,
-to run configured deduplication of student-assessment records based on configuration in the dbt project.
+{# MACRO FOR ASSESSMENT-SPECIFIC RULES
 
-For example, the partner may want to dedupe a state assessment by stu/assess/school/year, while deduping NWEA map by stu/assess/school/year/date
+This macro takes assessment-specific configuration inputs and generates deduplication code for student-assessment records.
 
-This macro returns SQL code with CTEs that can slot into any dbt model where `fct_student_assessment` and `dim_assessment`
-have been included as prior CTEs.
+Certain assessments may need different rules for deduplication. 
+For example, the partner may want to dedupe a state assessment by stu/assess/school/year, while deduping NWEA MAP by 
+stu/assess/school/year/date.
 
-TODO: review this method of macros that create partial code. Is that confusing, is it bad programming practice?
+TODO: should we have a non-by_assess version? Leaving out for now, bc it would basically just be recreating dbt_utils.deduplicate.
+But we could make it a wrapper that adds dedupe_rule as a column, similar to sch_assoc_rule in stu_assess_school_assoc().
+And, we could consider having pre-built rules for people to quickly implement, e.g. 'max_scale_score_by_stu_assess' -- 
+not sure how much value that would add, though.
 
 TODO: are we sure that rules will always be at level of assess_id & namespace? what if someone wants different rules by year or by tenant?
-    Could we make more flexible by adding arguments to the macro?
-#}
-{% macro stu_assess_dedupe(stu_assess_relation='fct_student_assessment',
-                           dedupe_rules=var('edu:assessment_reporting:dedupe_rules', {})) %}
+ Could we make more flexible by adding arguments to the macro?
 
+#}
+{% macro stu_assess_dedupe__by_assess(stu_assess_relation,
+                                      rules_by_assess={},
+                                      default_rules={}) %}
+
+
+{# LOOP over config, create dedupe CTE for each assessment #}
 {# default to empty string '', so if none configured, sql below compiles to `not in ('')`, which is valid and should return all rows 
         is there a cleaner way to handle this? #}
-{%- set rules_applied = ["''"] %} 
-  with
-{%- for key, rules_dict in dedupe_rules.items() %}
-  fct_student_assessment_{{key}} as (
-      select 
-        fct_student_assessment.*
-      from {{ stu_assess_relation }} fct_student_assessment
-      join {{ ref('dim_assessment') }} dim_assessment
-        on fct_student_assessment.k_assessment = dim_assessment.k_assessment
-      where dim_assessment.assessment_identifier = '{{ rules_dict["assessment_identifier"] }}'
-        and dim_assessment.namespace = '{{ rules_dict["namespace"] }}'
+{%- set rules_applied = ["''"] -%} 
+with
+{%- for key, rules_dict in rules_by_assess.items() %}
+  stu_assess_dedupe_{{key}} as (
+    select 
+      stu_assess.*
+    from {{ stu_assess_relation }} stu_assess
+    join {{ ref('dim_assessment') }} dim_assessment
+      on stu_assess.k_assessment = dim_assessment.k_assessment
+    where dim_assessment.assessment_identifier = '{{ rules_dict["assessment_identifier"] }}'
+      and dim_assessment.namespace = '{{ rules_dict["namespace"] }}'
   ),
   deduped_{{key}} as (
-      {%- set this_relation = 'fct_student_assessment_'~key -%}
-      {{
-          dbt_utils.deduplicate(
-              relation=this_relation,
-              partition_by=rules_dict["partition_by"],
-              order_by=rules_dict["order_by"]
-          )
-      }}
-  ){%- if not loop.last -%},{%- endif -%}
+    {%- set this_relation = 'stu_assess_dedupe_'~key -%}
+    {{
+        dbt_utils.deduplicate(
+            relation=this_relation,
+            partition_by=rules_dict["partition_by"],
+            order_by=rules_dict["order_by"]
+        )
+    }}
+  ),
   {%- set _ = rules_applied.append("'{}__{}'".format(rules_dict['assessment_identifier'], rules_dict['namespace'])) -%}
 
 {%- endfor %}
-  
-  {% for key in dedupe_rules %}
+  stu_assess_dedupe_default as (
+    select 
+      stu_assess.*
+    from {{ stu_assess_relation }} stu_assess
+    join {{ ref('dim_assessment') }} dim_assessment
+      on stu_assess.k_assessment = dim_assessment.k_assessment
+    where dim_assessment.assessment_identifier||'__'||dim_assessment.namespace not in ({{rules_applied|join(',')}})
+  )
+{# STACK each dedupe CTE from loop #}
+{% for key in rules_by_assess %}
   select *
   from deduped_{{key}}
   union all
-  {% endfor -%}
-  {# stack with assessments that don't have a dedupe rule configured #}
-  {# todo: add a section for default (x-assessment) dedupe rule? should that be allowed if there's assess-specific rules configured? #}
-  select 
-    fct_student_assessment.*
-  from {{ stu_assess_relation }} fct_student_assessment
-  join {{ ref('dim_assessment') }} dim_assessment
-    on fct_student_assessment.k_assessment = dim_assessment.k_assessment
-  where dim_assessment.assessment_identifier||'__'||dim_assessment.namespace not in ({{rules_applied|join(',')}})
-  
+{% endfor -%}
+  {# STACK with default: assessments that don't have a dedupe rule configured #}
+  {%- if default_rules %}
+    {{
+        dbt_utils.deduplicate(
+            relation='stu_assess_dedupe_default',
+            partition_by=default_rules["partition_by"],
+            order_by=default_rules["order_by"]
+        )
+    }}
+  {%- else %}
+    select * from stu_assess_dedupe_default
+  {%- endif -%}
+
 {% endmacro %}
