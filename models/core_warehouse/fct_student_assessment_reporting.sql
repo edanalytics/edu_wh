3828b91775ff707aaa390ba6e2906f878f943dba
rlittle08 <rlittle@edanalytics.org>
2024-01-24 14:58:10 -0600

    temporary - example model updates

--- models/core_warehouse/fct_student_assessment_reporting.sql
+++ models/core_warehouse/fct_student_assessment_reporting.sql
@@ -33,21 +33,87 @@ Cons:
 with fct_student_assessment as (
     select * from {{ ref('fct_student_assessment') }}
 ),
-stu_assess_sch as (
-  {# call macro to association stu-assess to schools, based on config in dbt project #}
-  {{stu_assess_school_assoc(stu_assess_relation='fct_student_assessment')}}
+fct_student_school_association as (
+    select * from {{ ref('fct_student_school_association') }}
+),
+xwalk_seasons as (
+  select * from {{ ref('xwalk_assessment_seasons') }}
+),
+xwalk_grade_levels as (
+  select * from {{ ref('xwalk_assessment_grade_levels') }}
+),
+xwalk_pl_values as (
+  select * from {{ ref('xwalk_assessment_performance_level_values') }}
+),
+xwalk_pl_thresholds as (
+  select * from {{ ref('xwalk_assessment_performance_level_thresholds') }}
 ),
 stu_assess_dedupe as (
   {# call macro to run dedupe, based on config in dbt project #}
-  {{stu_assess_dedupe(stu_assess_relation='stu_assess_sch')}}
+  {{ 
+    stu_assess_dedupe__by_assess(stu_assess_relation='fct_student_assessment',
+                                 rules_by_assess={
+                                  'nwea_map': {
+                                    'assessment_identifier':'NWEA-Map',
+                                    'namespace':'uri://www.nwea.org/map/Assessment',
+                                    'partition_by':'k_student,k_assessment,school_year,v_other_results:"Test Name",v_other_results:"Term Name"',
+                                    'order_by':'scale_score desc'
+                                  }
+                                 },
+                                 default_rules={
+                                  'partition_by': 'k_student,k_assessment',
+                                  'order_by': 'scale_score desc'
+                                 }) 
+  }} 
 ),
-{# call macro to add assessment season #}
-{{stu_assess_seasons(stu_assess_relation='stu_assess_dedupe')}}
-,
-{# call macro to add assessment grade level, based on config in dbt project #}
-{{stu_assess_grade_levels(stu_assess_relation='stu_assess_seasons')}}
-,
-{# call macro to add assessment performance level columns #}
-{{stu_assess_performance_levels(stu_assess_relation='stu_assess_grade_levels')}}
+stu_assess_sch as (
+  {# call macro to association stu-assess to schools, based on config in dbt project #}
+  {# {{stu_assess_school_assoc(stu_assess_relation='stu_assess_dedupe', #}
+                            {# stu_sch_relation='fct_student_school_association',)}} #}
+  {{
+    stu_assess_school_assoc__by_assess(stu_assess_relation='fct_student_assessment',
+                                       stu_sch_relation='fct_student_school_association',
+                                       rules_by_assess={
+                                         'nwea_map': {
+                                           'assessment_identifier': 'NWEA-Map',
+                                           'namespace': 'uri://www.nwea.org/map/Assessment',
+                                           'join_rule': 'stu_assess.k_student = stu_sch.k_student
+                                                         and stu_sch.is_latest_annual_entry',
+                                           'rule_name': 'latest_entry',
+                                           'dedupe_partition_by': 'k_student_assessment',
+                                           'dedupe_order_by': 'entry_date,exit_withdraw_date desc'
+                                         }
+                                       })
+  }}
+                            
+),
+stu_assess_seasons as (
+  {# call macro to add assessment season #}
+  {{
+    stu_assess_seasons(
+      stu_assess_relation='stu_assess_sch',
+      xwalk_seasons_relation='xwalk_seasons'
+    )
+  }}
+),
+stu_assess_grade_levels as (
+  {# call macro to add assessment grade level, based on config in dbt project #}
+  {{
+    stu_assess_grade_levels(
+      stu_assess_relation='stu_assess_seasons',
+      xwalk_grade_levels_relation='xwalk_grade_levels'
+    )
+  }}
+),
+stu_assess_performance_levels as (
+  {# call macro to add assessment performance level columns #}
+  {{
+    stu_assess_performance_levels(
+      stu_assess_relation='stu_assess_grade_levels',
+      xwalk_pl_values_relation='xwalk_pl_values',
+      xwalk_pl_thresholds_relation='xwalk_pl_thresholds'
+    ) 
+  }}
+)
 
-select * from stu_assess_pls
+select * from stu_assess_performance_levels
