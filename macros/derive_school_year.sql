117fc8bf19ff1a63f57f665dbbcb9a4e0d429c69
Jules <jalvord19@gmail.com>
2024-03-15 16:25:02 -0500

    clean up macros

--- macros/derive_school_year.sql
+++ macros/derive_school_year.sql
@@ -1,19 +1,10 @@
 {# 
 This macro takes derives school years from a date column.
+
+The date column will be compared to the default school year start day and month.
 #}
 
 {% macro derive_school_year(date_column) %}
-
-    {# {% set date_lower_bound = 
-            "concat_ws('/', '{{var('edu:school_year:start_month')}}', 
-                           '{{var('edu:school_year:start_day')}}', 
-                           year(date_column::date))::date" %}
-
-    {% set date_upper_bound = 
-            "concat_ws('/', '{{var('edu:school_year:start_month')}}', 
-                           '{{var('edu:school_year:start_day')}}', 
-                           (year(date_column::date)::int + 1))::date" %} #}
-
     case
         when {{date_column}}::date >= 
             concat_ws('/', '{{var("edu:school_year:start_month")}}', 
@@ -26,5 +17,4 @@ This macro takes derives school years from a date column.
             then (year({{date_column}}::date)::int + 1)
         else year({{date_column}}::date)
     end
-
 {% endmacro %}
