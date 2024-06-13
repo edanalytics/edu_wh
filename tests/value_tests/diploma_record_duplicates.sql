d2dea69a1cfa1c1b8038ac9e3275b4e2ff0fab9d
rlittle08 <rlittle@edanalytics.org>
2024-06-13 10:59:34 -0500

    extra note on the test

--- tests/value_tests/diploma_record_duplicates.sql
+++ tests/value_tests/diploma_record_duplicates.sql
@@ -1,18 +1,22 @@
 /*
 **What is this test?**
-This test finds records where the same diploma type is repeated across every term. 
+This test finds records where the same diploma type is repeated across multiple terms. 
 This test shows how the deduplication is being handled in fct_student_diploma, but the duplicates
 may point to data quality issues that could be addressed in the source system or ODS.
 
 **When is this important to resolve?**
 This data came from student academic records, which are linked to academic terms. This means 
-that the same diploma data is duplicated on every academic term of the school year when it 
+that the same diploma data is duplicated on every multiple academic terms of the school year when it 
 was awarded. If this test determines that a diploma has been duplicated many times over, it 
-is important to resolve this data issue so to avoid a warehouse model from blowing up. 
+is important to determine whether the default deduplication rule is operating correctly. The default
+rule is to sort on academic_term and take the first term alphabetically - this works well if the data are 
+exact duplicates and term is irrelevant. However, you may want to implement xwalk_academic_terms with a sort_index,
+if you prefer to keep e.g. "Full Year" term records in the deduplication.
 
 **How to resolve?**
 Determine whether this test has uncovered the behavior of these duplicates. 
-Depending on the diagnosis from the underlying data, you may raise issue in the source system.  
+Depending on the diagnosis from the underlying data, you may raise issue in the source system, or implement xwalk_academic_terms
+for a different deduplication rule.
 
 */
 
