8884254c31292d39fcb170361a8c69fb4c4996dc
keen <51969358+keenzarate213@users.noreply.github.com>
2024-06-17 17:01:10 -0500

    add earned_additional_credits in fct_course_transcript (#131)
    * add earned_additional_credits in fct_course_transcript
    
    * remove flatten and switch to variant instead. Add it description for this variant in the yml

--- models/core_warehouse/fct_course_transcripts.sql
+++ models/core_warehouse/fct_course_transcripts.sql
@@ -45,7 +45,8 @@ formatted as (
         course_transcripts.attempted_credit_type,
         course_transcripts.attempted_credit_conversion,
         course_transcripts.assigning_organization_identification_code,
-        course_transcripts.course_catalog_url
+        course_transcripts.course_catalog_url,
+        course_transcripts.v_earned_additional_credits
     from course_transcripts
     join fct_student_academic_record
         on course_transcripts.k_student_academic_record = fct_student_academic_record.k_student_academic_record
