11a1d591eae5a425ed600336533ba90bc540a8f7
rlittle08 <rlittle@edanalytics.org>
2024-06-18 13:36:35 -0500

    strict define current gpas structure to match

--- models/build/edfi_3/bld_ef3__combine_gpas.sql
+++ models/build/edfi_3/bld_ef3__combine_gpas.sql
@@ -4,6 +4,17 @@ with current_gpas as (
 deprecated_gpas as (
     select * from {{ ref('stg_ef3__student_academic_records') }}
 ),
+format_current_gpas as (
+    select
+        tenant_code,
+        api_year,
+        k_student_academic_record,
+        gpa_type,
+        gpa_value,
+        is_cumulative,
+        max_gpa_value
+    from current_gpas
+),
 format_deprecated_gpas as (
     select 
         tenant_code,
