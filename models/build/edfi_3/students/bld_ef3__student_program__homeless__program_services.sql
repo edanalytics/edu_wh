3e913cfcb611c1bfa8015f5689ae1b81689f5e66
Steven McKee <steven.mckee@tnedu.gov>
2024-10-16 11:54:51 -0500

    Modified a couple of blds for databricks syntax instead of snowflake.

--- models/build/edfi_3/students/bld_ef3__student_program__homeless__program_services.sql
+++ models/build/edfi_3/students/bld_ef3__student_program__homeless__program_services.sql
@@ -12,10 +12,22 @@ wide as (
         k_lea,
         k_school,
         program_enroll_begin_date,
-        array_agg(program_service) within group (order by program_service) as program_services
-
-    from stage_program_services
-
+        array_agg(program_service) as program_services
+    from (
+        select distinct
+            tenant_code,
+            api_year,
+            k_student,
+            k_student_xyear,
+            k_program,
+            k_lea,
+            k_school,
+            program_enroll_begin_date,
+            program_service
+        from stage_program_services
+        order by program_service
+    )
+    
     {{ dbt_utils.group_by(n=8) }}
 )
 
