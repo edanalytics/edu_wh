cb837cbc2c1a1b50cadbe84a9ebd24565b8b5718
sleblanc23 <sleblanc@edanalytics.org>
2025-04-28 16:53:21 -0500

    example of config method

--- models/core_warehouse/fct_student_gpa.sql
+++ models/core_warehouse/fct_student_gpa.sql
@@ -1,13 +1,3 @@
-{{
-  config(
-    post_hook=[
-        "alter table {{ this }} add primary key (k_student_academic_record, gpa_type, is_cumulative)",
-        "alter table {{ this }} add constraint fk_{{ this.name }}_academic_record foreign key (k_student_academic_record) references {{ ref('fct_student_academic_record') }}",
-        "alter table {{ this }} add constraint fk_{{ this.name }}_student foreign key (k_student) references {{ ref('dim_student') }}",
-    ]
-  )
-}}
-
 with combined_gpas as (
     select * from {{ ref('bld_ef3__combine_gpas') }}
 ),
