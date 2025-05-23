4d800f67ebf03e643225351baf03a53e64e0a14b
sleblanc23 <sleblanc@edanalytics.org>
2025-05-23 15:57:10 -0500

    use macros for xplatform support - possibly return to clean this up

--- models/core_warehouse/dim_class_period.sql
+++ models/core_warehouse/dim_class_period.sql
@@ -23,23 +23,24 @@ formatted as (
         class_periods.school_year,
         class_periods.class_period_name,
         class_periods.is_official_attendance_period,
+        {{ edu_edfi_source.json_get_first('v_meeting_times', 'variant') }} as meeting_time,
         -- if there is only one meeting time, extract it, else leave null
         case when {{ edu_edfi_source.json_array_size('v_meeting_times') }} = 1
         then
             -- convert to military time for time math, if it isn't
             -- (assume class periods will not be scheduled between 1 and 6 AM)
             case 
-                when left(v_meeting_times[0].startTime::string, 2)::int between 1 and 6
-                then concat(left(v_meeting_times[0].startTime::string, 2)::int + 12, right(v_meeting_times[0].startTime::string, 6))
-                else v_meeting_times[0].startTime::string
+                when left({{ edu_edfi_source.json_ci_get('meeting_time', 'startTime') }}::string, 2)::int between 1 and 6
+                then concat(left({{ edu_edfi_source.json_ci_get('meeting_time', 'startTime') }}, 2)::int + 12, right({{ edu_edfi_source.json_ci_get('meeting_time', 'startTime') }}::string, 6))
+                else {{ edu_edfi_source.json_ci_get('meeting_time', 'startTime') }}::string
             end
         end as start_time,
         case when {{ edu_edfi_source.json_array_size('v_meeting_times') }} = 1
         then 
             case 
-                when left(v_meeting_times[0].endTime::string, 2)::int between 1 and 6
-                then concat(left(v_meeting_times[0].endTime::string, 2)::int + 12, right(v_meeting_times[0].endTime::string, 6))
-                else v_meeting_times[0].endTime::string
+                when left({{ edu_edfi_source.json_ci_get('meeting_time', 'endTime') }}::string, 2)::int between 1 and 6
+                then concat(left({{ edu_edfi_source.json_ci_get('meeting_time', 'endTime') }}::string, 2)::int + 12, right({{ edu_edfi_source.json_ci_get('meeting_time', 'endTime') }}::string, 6))
+                else {{ edu_edfi_source.json_ci_get('meeting_time', 'endTime') }}::string
             end
         end as end_time,
         timediff(MINUTE, concat('2020-01-01 ', start_time)::timestamp, concat('2020-01-01 ', end_time)::timestamp) as period_duration
@@ -63,5 +64,6 @@ formatted as (
       {% endfor %}
     {%- endif %}
 )
-select * from formatted
+select {{ edu_edfi_source.star('formatted', except=['meeting_time']) }}
+from formatted
 order by tenant_code, k_school, k_class_period
