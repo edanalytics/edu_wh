e2134671a3c0ed80c63964b395aaa4bddbbbbc4e
Julianna Alvord <jalvord19@gmail.com>
2025-11-10 08:57:31 -0600

    rename access table to cross tenant

--- models/build/edfi_3/assessments/bld_ef3__student_assessment_cross_tenant.sql
+++ models/build/edfi_3/assessments/bld_ef3__student_assessment_cross_tenant.sql
@@ -1,4 +1,10 @@
-{% if var("edu:assessments:assessment_access", False) -%}
+{% if var("edu:assessments:assessment_cross_tenant", False) -%}
+{# Load students to remove source from var #}
+{% set removed_students_source = var("edu:assessments:removed_students_source") %}
+
+-- if the student_unique_id is not global for a partner, then we need to use the other ID type for 
+{% set globally_unique_id = var("edu:assessments:unique_id") %}
+
 with dim_student as (
     select * from {{ ref('dim_student') }}
 ),
@@ -16,12 +22,39 @@ active_enrollments as (
         fct_student_school.tenant_code,
         fct_student_school.k_student,
         fct_student_school.k_student_xyear,
-        dim_student.student_unique_id
+        dim_student.student_unique_id,
+        {% if unique_id != 'student_unique_id' -%}
+        dim_student.{{unique_id}},
+        {% endif %}
+        dim_student.birth_date,
+        dim_student.first_name,
+        dim_student.last_name
     from fct_student_school
     join dim_student
         on fct_student_school.k_student = dim_student.k_student
+    -- if this source is configured, remove these students instead of using default logic below
+    {% if removed_students_source is not none and removed_students_source | length -%}
+    left join {{ ref(removed_students_source) }}
+        on dim_student.k_student = {{ removed_students_source }}.k_student
     where is_active_enrollment
+    and {{ removed_students_source }}.k_student is not null
+    {% else %}
+    where is_active_enrollment
+    -- below is default logic to ensure the chosen 'global' IDs actually represent the same student
+    -- only to be used if a students to remove source is not configured
+    qualify 
+        -- either the birthdates are the same
+        (count(distinct dim_student.birth_date) over (partition by dim_student.student_unique_id) <= 1
+            -- at least 1 of birthdate, first name, or last name must be the same
+            or (
+                count(distinct dim_student.birth_date) over (partition by dim_student.student_unique_id)
+                + count(distinct lower(dim_student.first_name)) over (partition by dim_student.student_unique_id)
+                + count(distinct lower(dim_student.last_name))  over (partition by dim_student.student_unique_id)
+            ) <= 5)
+        and count(distinct fct_student_school.tenant_code) over(partition by dim_student.student_unique_id) > 1
+    {% endif %}
 ),
+-- we need to use school enrollments to look at start and end dates, but we want the grain to be unique by student, year, and tenant
 deduped_enrollments as (
     {{
         dbt_utils.deduplicate(
@@ -42,7 +75,7 @@ subset_assessments as (
         -- TODO: this key may not join to anything
             -- but, can't leave original because of RLS
             -- create new records? complicated logic
-        {{ dbt_utils.generate_surrogate_key(
+        {{dbt_utils.generate_surrogate_k ey(
             ['deduped_enrollments.tenant_code',
             'deduped_enrollments.school_year',
             'lower(stg_student_assessment.academic_subject)',
@@ -66,7 +99,6 @@ subset_assessments as (
                 then 1
             else 0
         end as is_original_record
-        -- TODO: flag for original record
     from stg_student_assessment
     -- this code will intentionally create dupes to associate a student assessment record
     -- to every tenant where a current enrollment exists for that student_unique_id
@@ -82,7 +114,6 @@ subset_assessments as (
 
         -- TODO: configurable ID type
         on stg_student_assessment.student_unique_id = deduped_enrollments.student_unique_id
-        -- TODO: var to optionally remove tenant codes
 )
 select *
 from subset_assessments
