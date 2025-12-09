2c793b15e485decd8bb3c20f53cb60d5dfdbc908
Ryan Aguilar <raguilar@edanalytics.org>
2025-12-09 14:07:05 -0500

    Remove program name and type from assoc model

---
+++ models/tpdm_warehouse/fct_candidate_educator_preparation_program_association.sql
@@ -0,0 +1,44 @@
+{{
+  config(
+    post_hook=[
+        "alter table {{ this }} alter column k_candidate_educator_preparation_program_association set not null",
+        "alter table {{ this }} add primary key (k_candidate_educator_preparation_program_association)",
+        "alter table {{ this }} add constraint fk_{{ this.name }}_staff foreign key (k_candidate) references {{ ref('dim_candidate') }}",
+        "alter table {{ this }} add constraint fk_{{ this.name }}_school foreign key (k_educator_preparation_program) references {{ ref('dim_educator_preparation_program') }}",
+    ]
+  )
+}}
+
+with stage as (
+    select * from {{ ref('stg_tpdm__candidate_educator_preparation_program_associations') }}
+),
+
+dim_candidate as (
+    select * from {{ ref('dim_candidate') }}
+),
+
+dim_epp as (
+    select * from {{ ref('dim_educator_preparation_program') }}
+),
+
+formatted as (
+    select
+        dim_candidate.k_candidate,
+        dim_candidate.k_candidate_xyear,
+        stage.k_candidate_educator_preparation_program_association,
+        dim_epp.k_educator_preparation_program,
+        stage.tenant_code,
+        stage.school_year,
+        stage.begin_date,
+        stage.program_type,
+        stage.reason_exited,
+        stage.epp_program_pathway,
+        stage.v_degree_specializations,
+        stage.v_cohort_years,
+    from stage
+    join dim_epp
+        on stage.k_educator_preparation_program = dim_epp.k_educator_preparation_program
+    join dim_candidate
+        on stage.k_candidate = dim_candidate.k_candidate
+)
+select * from formatted
