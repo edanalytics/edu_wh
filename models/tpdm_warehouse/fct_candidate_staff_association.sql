0565b42e52aae8da2f21306a49a9a3551a1f29fa
rlittle08 <rlittle@edanalytics.org>
2025-12-09 12:40:45 -0600

    Create fct_candidate_staff_association.sql

---
+++ models/tpdm_warehouse/fct_candidate_staff_association.sql
@@ -0,0 +1,27 @@
+with stg_candidate_staff_association as (
+    select * 
+    from {{ ref('stg_tpdm__candidate_relationship_to_staff_associations') }}
+),
+dim_candidate as (
+    select * from {{ ref('dim_candidate') }}
+),
+dim_staff as (
+    select * from {{ ref('dim_staff') }}
+),
+{# inner joins to enforce referential integrity, candidates and staff must exist #}
+joined as (
+    select
+        stg_candidate_staff_association.k_candidate,
+        stg_candidate_staff_association.k_staff,
+        stg_candidate_staff_association.tenant_code,
+        stg_candidate_staff_association.api_year,
+        stg_candidate_staff_association.begin_date,
+        stg_candidate_staff_association.staff_to_candidate_relationship
+        {{ edu_edfi_source.extract_extension(model_name='stg_tpdm__candidate_relationship_to_staff_associations', flatten=False) }}
+    from stg_candidate_staff_association
+    join dim_candidate 
+      on stg_candidate_staff_association.k_candidate = dim_candidate.k_candidate
+    join dim_staff 
+      on stg_candidate_staff_association.k_staff = dim_staff.k_staff
+)
+select * from joined
