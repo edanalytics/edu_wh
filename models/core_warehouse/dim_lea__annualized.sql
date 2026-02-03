809a407883dd8f4e084b609c83b337282471268e
Keen Zarate <kzarate@edanalytics.org>
2026-02-03 15:18:34 -0600

    draft

---
+++ models/core_warehouse/dim_lea__annualized.sql
@@ -0,0 +1,67 @@
+{{
+  config(
+    post_hook=[
+        "alter table {{ this }} alter column k_lea set not null",
+        "alter table {{ this }} add primary key (k_lea)",
+    ]
+  )
+}}
+
+with stg_lea as (
+    select * from {{ ref('stg_ef3__local_education_agencies__annualized') }}
+),
+tenant_lea_ownership as (
+    select * from {{ ref('bld_ef3__tenant_lea_ownership') }}
+),
+choose_address as (
+    {{ row_pluck(ref('stg_ef3__local_education_agencies__addresses'),
+                key='k_lea',
+                column='address_type',
+                preferred='Physical',
+                where='address_end_date is null') }}
+),
+formatted as (
+    select 
+        stg_lea.k_lea,
+        {{ dbt_utils.generate_surrogate_key(
+            [
+                'stg_lea.k_lea',
+                'stg_lea.api_year'
+            ]
+        )}} as k_lea__annualized,
+        stg_lea.k_lea__parent,
+        stg_lea.k_sea,
+        {% if var('src:ed_orgs:esc:enabled', True) %}
+        stg_lea.k_esc,
+        {% endif %}
+        stg_lea.tenant_code,
+        stg_lea.lea_id,
+        stg_lea.lea_name,
+        stg_lea.lea_short_name,
+        stg_lea.parent_lea_id,
+        stg_lea.lea_category,
+        stg_lea.sea_id,
+        stg_lea.esc_id,
+        stg_lea.operational_status,
+        stg_lea.charter_status,
+        choose_address.address_type,
+        choose_address.street_address,
+        choose_address.city,
+        choose_address.name_of_county,
+        choose_address.state_code,
+        choose_address.postal_code,
+        choose_address.building_site_number,
+        choose_address.locale,
+        choose_address.congressional_district,
+        choose_address.county_fips_code,
+        choose_address.latitude,
+        choose_address.longitude
+    from stg_lea
+    left join choose_address 
+        on stg_lea.k_lea = choose_address.k_lea
+    join tenant_lea_ownership
+        on stg_lea.tenant_code = tenant_lea_ownership.tenant_code
+        and stg_lea.lea_id = tenant_lea_ownership.lea_id
+)
+select * from formatted
+order by tenant_code, k_lea
---
+++ models/core_warehouse/dim_school__annualized.sql
@@ -0,0 +1,134 @@
+-- depends_on: {{ ref('dim_network') }}
+{{
+  config(
+    post_hook=[
+        "alter table {{ this }} alter column k_school set not null",
+        "alter table {{ this }} add primary key (k_school)",
+
+        "{% set network_types = dbt_utils.get_column_values(
+                           table=ref('xwalk_network_association_types'),
+                           column='network_type',
+                           where=\"association_type = 'school'\",
+                           order_by='network_type')
+        %}
+        {% if network_types is not none %}
+        {% for network_type in network_types %} 
+            alter table {{ this }} add constraint fk_{{this.name}}_{{network_type}}_network foreign key (k_network__{{network_type}}) references {{ ref('dim_network') }} (k_network)
+            {%if not loop.last%};{%endif%}
+        {% endfor %}
+        {% endif %}"
+    ]
+  )
+}}
+{# Load custom data sources from var #}
+{% set custom_data_sources = var("edu:schools:custom_data_sources", []) %}
+
+with stg_school as (
+    select * from {{ ref('stg_ef3__schools__annualized') }}
+),
+bld_ef3__wide_ids_school as (
+    select * from {{ ref('bld_ef3__wide_ids_school') }}
+),
+bld_network_associations as (
+  select * from {{ ref('bld_ef3__wide_school_network_assoc')}}
+),
+choose_address as (
+    {{ row_pluck(ref('stg_ef3__schools__addresses'),
+                key='k_school',
+                column='address_type',
+                preferred='Physical',
+                where='address_end_date is null') }}
+),
+dim_lea as (
+    select * from {{ ref('dim_lea__annualized') }}
+),
+formatted as (
+    select 
+        stg_school.k_school,
+        {{ dbt_utils.generate_surrogate_key(
+            [
+                'stg_school.k_school',
+                'stg_school.api_year'
+            ]
+        )}} as k_school_annualized,
+        stg_school.k_lea,
+        stg_school.api_year as school_year,
+        -- if there are any networks configured for school assoc, add those foreign keys as k_network__{network_type}
+        {% set network_types = dbt_utils.get_column_values(
+                           table=ref('xwalk_network_association_types'),
+                           column='network_type',
+                           where="association_type = 'school'",
+                           order_by='network_type')
+        %}
+        {%- if network_types is not none and network_types | length -%}
+          {%- for network_type in network_types -%}
+            bld_network_associations.k_network__{{network_type}}::varchar(32) as k_network__{{network_type}},
+          {%- endfor -%}
+        {%- endif %}
+        stg_school.tenant_code,
+        stg_school.school_id,
+        {{ accordion_columns(
+            source_table='bld_ef3__wide_ids_school',
+            exclude_columns=['tenant_code', 'k_school']) }}
+        stg_school.school_name,
+        stg_school.school_short_name,
+        dim_lea.lea_name,
+        dim_lea.lea_id,
+        stg_school.school_category,
+        stg_school.school_type,
+        stg_school.operational_status,
+        stg_school.administrative_funding_control,
+        stg_school.internet_access,
+        stg_school.title_i_part_a_school_designation,
+        stg_school.charter_status,
+        stg_school.charter_approval_agency,
+        stg_school.magnet_type,
+
+        -- custom indicators
+        {% set custom_indicators = var('edu:schools:custom_indicators') %}
+        {%- if custom_indicators is not none and custom_indicators | length -%}
+          {%- for indicator in custom_indicators -%}
+              {{ custom_indicators[indicator]['where'] }} as {{ indicator }},
+          {%- endfor -%}
+        {%- endif %}
+        stg_school.website,
+        choose_address.address_type,
+        choose_address.street_address,
+        choose_address.city,
+        choose_address.name_of_county,
+        choose_address.state_code,
+        choose_address.postal_code,
+        choose_address.building_site_number,
+        choose_address.locale,
+        choose_address.congressional_district,
+        choose_address.county_fips_code,
+        choose_address.latitude,
+        choose_address.longitude
+
+        -- custom data sources
+        {% if custom_data_sources is not none and custom_data_sources | length -%}
+          {%- for source in custom_data_sources -%}
+            {%- for indicator in custom_data_sources[source] -%}
+              , {{ custom_data_sources[source][indicator]['where'] }} as {{ indicator }}
+            {%- endfor -%}
+          {%- endfor -%}
+        {%- endif %}
+    from stg_school
+    join dim_lea 
+        on stg_school.k_lea = dim_lea.k_lea
+    left join bld_ef3__wide_ids_school
+        on stg_school.k_school = bld_ef3__wide_ids_school.k_school
+    left join choose_address
+        on stg_school.k_school = choose_address.k_school
+    left join bld_network_associations
+        on stg_school.k_school = bld_network_associations.k_school
+    -- custom data sources
+    {% if custom_data_sources is not none and custom_data_sources | length -%}
+      {%- for source in custom_data_sources -%}
+        left join {{ ref(source) }}
+          on stg_school.k_school = {{ source }}.k_school
+      {% endfor %}
+    {%- endif %}
+)
+select * from formatted
+order by tenant_code, k_school
