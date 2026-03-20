ea7dea73061fba7025c9b535f92fe8fa155260de
rlittle08 <rlittle@edanalytics.org>
2026-03-20 14:03:32 -0500

    collapse into existing cte to avoid unnecessary extra lines

--- models/build/edfi_3/courses/bld_ef3__course_char__combined_wide.sql
+++ models/build/edfi_3/courses/bld_ef3__course_char__combined_wide.sql
@@ -5,26 +5,16 @@ with char_long as (
 xwalk_course_char as (
     select * from {{ ref('xwalk_course_level_characteristics')}}
 ),
--- Human-readable list of all descriptors present for the section (course / offering / section levels).
--- Uses edu_edfi_source.json_array_agg for Snowflake vs Databricks array semantics.
-descriptor_array as (
-    select
-        tenant_code,
-        api_year,
-        k_course_section,
-        {{ edu_edfi_source.json_array_agg(
-            'distinct course_level_characteristic',
-            order_by='course_level_characteristic',
-            is_terminal=False
-        ) }} as course_level_characteristics_array
-    from char_long
-    group by tenant_code, api_year, k_course_section
-),
 pivoted as (
     select 
         tenant_code,
         api_year,
-        k_course_section
+        k_course_section,
+        {{ edu_edfi_source.json_array_agg(
+            'course_level_characteristic',
+            order_by='course_level_characteristic',
+            is_terminal=False
+        ) }} as course_level_characteristics_array
         {%- if not is_empty_model('xwalk_course_level_characteristics') -%},
           {{ ea_pivot(
                 column='indicator_name',
@@ -37,11 +27,4 @@ pivoted as (
         on char_long.course_level_characteristic = xwalk_course_char.characteristic_descriptor
     group by all
 )
-select
-    pivoted.*,
-    descriptor_array.course_level_characteristics_array
-from pivoted
-join descriptor_array
-    on pivoted.tenant_code = descriptor_array.tenant_code
-    and pivoted.api_year = descriptor_array.api_year
-    and pivoted.k_course_section = descriptor_array.k_course_section
+select * from pivoted
