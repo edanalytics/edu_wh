1b2868650cf2bc8dc42c76821cb3649c8d263e71
Ryan Aguilar <raguilar@edanalytics.org>
2026-03-26 14:12:52 -0400

    Add fct_certificationm_exam

---
+++ models/tpdm_warehouse/fct_certification_exam_result.sql
@@ -0,0 +1,26 @@
+{{
+  config(
+    post_hook=[
+        "alter table {{ this }} alter column k_certification_exam_result set not null",
+    ]
+  )
+}}
+
+with stage as (
+    select * from {{ ref('stg_tpdm__certification_exam_results') }}
+),
+
+formatted as (
+    select
+        stage.k_certification_exam_result,
+        stage.k_person,
+        stage.k_certification_exam,
+        stage.tenant_code,
+        stage.school_year,
+        stage.certification_exam_date,
+        stage.attempt_number,
+        stage.certification_exam_pass_indicator,
+        stage.person_source_system_descriptor
+    from stage
+)
+select * from formatted
