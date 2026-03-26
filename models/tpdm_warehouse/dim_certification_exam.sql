4ede782f234a72d3f23a3c641511f9e6c3e00d95
Ryan Aguilar <raguilar@edanalytics.org>
2026-03-26 13:44:59 -0400

    Add dim_certification_exam

---
+++ models/tpdm_warehouse/dim_certification_exam.sql
@@ -0,0 +1,26 @@
+{{
+  config(
+    post_hook=[
+        "alter table {{ this }} alter column k_certification_exam set not null",
+        "alter table {{ this }} add primary key (k_certification_exam)",
+    ]
+  )
+}}
+
+with stg_exam as (
+    select * from {{ ref('stg_tpdm__certification_exams') }}
+),
+
+formatted as (
+    select
+        stg_exam.k_certification_exam,
+        stg_exam.tenant_code,
+        stg_exam.school_year,
+        stg_exam.certification_exam_id,
+        stg_exam.certification_exam_title,
+        stg_exam.certification_exam_type,
+        stg_exam.effective_date,
+        stg_exam.end_date
+    from stg_exam
+)
+select * from formatted
